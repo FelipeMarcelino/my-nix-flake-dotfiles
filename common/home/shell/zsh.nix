22d091bdf838040157eda510650babb19f3c4077
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2023-12-24 08:37:11 -0300

    Shell zsh (#5)
    * zsh configuration first commit
    
    * hash and some oh my zsh plugins
    
    * ctrl-space accept suggest
    
    * zsh configured most of the parts
    
    * removing commented code

--- common/home/shell/zsh.nix
+++ common/home/shell/zsh.nix
@@ -1,5 +1,7 @@
 { config, pkgs, ...}:
 {
+   programs.zsh.enable = true;
+   programs.zsh.enableCompletion = true;
    programs.fzf.enableZshIntegration = true;
    programs.nix-index.enableZshIntegration = true;
    programs.skim.enableZshIntegration = true;
@@ -13,5 +15,34 @@
    programs.zsh.oh-my-zsh.enable = true;
    programs.zsh.syntaxHighlighting.enable = true;
    programs.zsh.zsh-abbr.enable = true;
+   programs.hstr.enable = true;
+   programs.hstr.enableZshIntegration = true;
+   programs.dircolors.enableZshIntegration = true;
+   programs.zsh.defaultKeymap = "viins";
+   programs.zsh.dirHashes = {
+	docs = "$HOME/Documents";
+	vids = "$HOME/Videos";
+	dl = "$HOME/Downloads";
+	pics = "$HOME/Pictures";
+	dev = "$HOME/Development";
+	mus = "$HOME/Music";
+	dot = "$HOME/Development/nix-dotfiles";
+   };
+   programs.zsh.history.extended = true;
+   programs.zsh.historySubstringSearch.searchDownKey = [ ];
+   programs.zsh.historySubstringSearch.searchUpKey = [ ];
+   programs.zsh.oh-my-zsh.plugins = [ "sudo" "git" "vi-mode" "scala" "sbt" "ripgrep" 
+	"python" "colored-man-pages" "cp" "aliases" "alias-finder" "common-aliases" ];
 
+   programs.zsh.initExtra = ''
+bindkey '^ ' autosuggest-accept
+   '';
+
+  programs.zsh.loginExtra = ''
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
+  '';
+  programs.zsh.shellAliases = {
+   run = "nix run .#activate";
+  };
 }
