c6ccbe43817b9fad669b9f69103c08bfde5a2fd0
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2023-12-28 17:10:30 -0300

    default and association included. Sign

--- linux/solid/home/xdg/default.nix
+++ linux/solid/home/xdg/default.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ...}:
 {
    xdg.mimeApps.enable = true;
+   xdg.mime.enable = true;
    xdg.userDirs = {
    	extraConfig = {
 	   XDG_DEV_DIR = "${config.home.homeDirectory}/Development";
@@ -11,4 +12,15 @@
    imports = [
 	./augmented-open.nix
    ];
+
+   xdg.mimeApps.defaultApplications = 
+   {
+	"x-scheme-handler/file-line-column" = [ "AugmentedOpen.desktop" ];
+   };
+
+   xdg.mimeApps.associations.added = 
+   {
+	"x-scheme-handler/file-line-column" = [ "AugmentedOpen.desktop" ];
+   };
+
 }
