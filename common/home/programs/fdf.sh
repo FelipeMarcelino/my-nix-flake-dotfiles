c2fde06a813d95914bd19597eef8c2bf244e9b71
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2023-12-29 17:27:07 -0300

    fdf

--- common/home/programs/fdf.sh
+++ common/home/programs/fdf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-fd --glob '*' --type d  | fzf --prompt '>Directories> ' \
+fd --glob "*" --type d  | fzf --prompt  '>Directories> ' \
              --header 'CTRL-D: Directories / CTRL-F: Files' \
-             --bind 'ctrl-d:change-prompt(Directories> )+reload(fd --glob '*' --type d)' \
-             --bind 'ctrl-f:change-prompt(Files> )+reload(fd --glob '*' --type f)'
+             --bind 'ctrl-d:change-prompt(Directories> )+reload(fd --glob "*" --type d)' \
+             --bind 'ctrl-f:change-prompt(Files> )+reload(fd --glob "*" --type f)' \
