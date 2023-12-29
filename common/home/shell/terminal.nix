550b94a9025de5b1767d2637a9fcdb496d5a61c6
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2023-12-29 20:39:31 -0300

    terminfo wezterm

--- common/home/shell/terminal.nix
+++ common/home/shell/terminal.nix
@@ -17,12 +17,15 @@ config.audible_bell = "Disabled"
 config.automatically_reload_config = true
 config.default_cursor_style = "BlinkingBlock"
 config.animation_fps = 20
-config.font_antialias = "Subpixel"
-config.font_hinting = "Full"
+config.term = "wezterm"
 config.hide_tab_bar_if_only_one_tab = true
 config.show_new_tab_button_in_tab_bar = false
 config.show_tabs_in_tab_bar = false
 config.switch_to_last_active_tab_when_closing_tab = true
+config.set_environment_variables = {
+  TERMINFO_DIRS = '/home/user/.nix-profile/share/terminfo',
+  WSLENV = 'TERMINFO_DIRS',
+}
 config.skip_close_confirmation_for_processes_named = {
    "zsh",
    "bash",
