b09caea36ee3729933ab05217ff3073c0978aa65
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2023-12-30 11:33:03 -0300

    theme package in nix way

--- linux/solid/home/rofi/default.nix
+++ linux/solid/home/rofi/default.nix
@@ -3,5 +3,13 @@
 {
     programs.rofi = {
 	enable = true;
+	terminal = "wezterm";
+	theme = "./rofi/files/launchers/type-1/style-6.rasi";
     };
+
+    # for rofi-emoji
+    home.packages = [ 
+    	pkgs.xdotool 
+	(import ./rofi-themes.nix)
+    ];
 }
---
+++ linux/solid/home/rofi/rofi-themes.nix
@@ -0,0 +1,22 @@
+{ lib, stdenv, fetchFromGitHub, bash}:
+
+stdenv.mkDerivation {
+   name = "rofi-themes";
+   src = fetchFromGitHub {
+	owner = "adi1090x";
+	repo = "rofi";
+	rev = "master";
+	sha256 = "sha256-YjyrxappcLDoh3++mtZqCyxQV2qeoNhhUy2XGwlyTng=";
+   };
+
+   buildInputs = [ bash ];
+
+   installPhase = ''
+   	mkdir -p $out/bin
+	cp setup.sh $out/bin/setup.sh 
+	cp -rf files $out/bin/files
+	cp -rf fonts $out/bin/fonts
+	cp -rf previews $out/bin/previews
+	bash $out/bin/setup.sh
+   '';
+}
