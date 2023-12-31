1017e3ca42daccf0c32cbc76e22c35bd2889a69c
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2023-12-31 00:19:28 -0300

    flake packages

---
+++ linux/solid/home/rofi/rofi_themes/default.nix
@@ -0,0 +1,26 @@
+{ lib, pkgs, ...}:
+{
+   pname = "rofi-themes";
+   src = pkgs.fetchFromGitHub {
+	owner = "adi1090x";
+	repo = "rofi";
+	rev = "master";
+	sha256 = "sha256-YjyrxappcLDoh3++mtZqCyxQV2qeoNhhUy2XGwlyTng=";
+   };
+
+   buildPhase = ''
+	mkdir -p $out
+	chmod +x $src/setup.sh
+   '';
+
+   buildInputs = [ pkgs.bash ];
+
+   installPhase = ''
+	bash $src/setup.sh 
+	cp -rf $src/* $out/
+	echo '#!/bin/sh' > $out/bin/rofi-themes
+	chmod +x $out/bin/rofi-themes
+   '';
+
+   #phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+}
--- linux/solid/home/rofi/rofi_themes/flake.nix
+++ linux/solid/home/rofi/rofi_themes/flake.nix
@@ -1,39 +1,14 @@
 {
- outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.rofi-themes =
-      let pkgs = import nixpkgs {
-            system = "x86_64-linux";
-          };
-      in pkgs.stdenv.mkDerivation {
-	   name = "rofi-themes";
-	   src = pkgs.fetchFromGitHub {
-		owner = "adi1090x";
-		repo = "rofi";
-		rev = "master";
-		sha256 = "sha256-YjyrxappcLDoh3++mtZqCyxQV2qeoNhhUy2XGwlyTng=";
-	   };
+   inputs = {
+   	nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";	
+	flake-parts.url = "github:hercules-ci/flake-parts";
+   };
 
-	   buildPhase = ''
-		export FONTCONFIG_FILE=$XDG_CONFIG_HOME/fontconfig/conf.d/10-hm-fonts.conf
-		export FONTCONFIG_PATH=$XDG_CONFIG_HOME/fontconfig/conf.d
-		export HOME=$(pwd)
-		mkdir -p $out
-		chmod +x $src/setup.sh
-	   '';
+   outputs = inputs@{ self, nixpkgs, flake-parts, ... }: 
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
 
-	   buildInputs = [ pkgs.bash pkgs.fontconfig ];
-	   #buildInputs = [ pkgs.bash ];
-
-	   installPhase = ''
-		bash $src/setup.sh 
-		cp -rf $src/* $out/
-		echo '#!/bin/sh' > $out/bin/rofi-themes
-		chmod +x $out/bin/rofi-themes
-	   '';
-
-	   phases = [ "unpackPhase" "buildPhase" "installPhase" ];
-    };
-    # Specify the default package
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.rofi-themes; # <- add this
-  };
+      packages.rofi-themes = flake-parts.packages.rofi-themes;
+   };
 }
+
