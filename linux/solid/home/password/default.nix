631383097acb8e1a8317efcf2689cfd9f7367903
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2024-01-02 23:03:37 -0300

    keyring password configured and bw working, but not rbw

--- linux/solid/home/password/default.nix
+++ linux/solid/home/password/default.nix
@@ -2,9 +2,16 @@
 {
    # Keyring/Password Manager
    services.gnome-keyring.enable = true;
+   services.gnome-keyring.components = ["pkcs11" "secrets" "ssh"];
    programs.rbw.enable = true;
+   programs.rbw.settings.pinentry = "curses";
    programs.rbw.settings.email = "felipe.marcelino1991@gmail.com";
    programs.rbw.settings.base_url = "https://api.bitwarden.com/";
    programs.rbw.settings.identity_url = "https://identity.bitwarden.com/";
+
+   home.packages = [
+	pkgs.bitwarden-cli
+	pkgs.libsecret
+   ];
 }
 
