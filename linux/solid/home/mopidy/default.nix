b7aec424d3afd7e6361813a31c554393437d49ab
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2024-01-03 07:20:07 -0300

    mopidy keyring added with bash script.

--- linux/solid/home/mopidy/default.nix
+++ linux/solid/home/mopidy/default.nix
@@ -8,8 +8,23 @@
 	   	enabled = true;
 		client_id = "6603cc0f-9582-4480-9b1a-17e2ec102318";
 		client_secret = "CkFb_nvQPWWHsdv-hE9QrdtGuP2RhEUp1JPdk3PUrgE=";
+		username = "$SPOTIFY_USERNAME";
+		password = "$SPOTIFY_PASSWORD";
 		bitrate = 320;
 	   };
 	};
+   };
+
+   systemd.user.services."def-spotify-username-password" = {
+	Unit = {
+	   After = [ "network.target" "sound.target" ];
+	   Before = [ "mopidy.service" ];
+	   Description = "Get username and password from Gnome Keyring";
+	};
+
+	Service = {
+	   ExecStart = "/home/felipemarcelino/Development/nix-dotfiles/linux/solid/home/mopidy/mopidy.sh";
+	};
+
    };
 }
