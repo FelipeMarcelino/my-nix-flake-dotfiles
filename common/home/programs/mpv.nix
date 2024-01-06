204421d6a004674e779c2831fd0913390e8452c5
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2024-01-06 03:34:16 -0300

    adding linux configuration files for mpv

--- common/home/programs/mpv.nix
+++ common/home/programs/mpv.nix
@@ -1,7 +1,15 @@
 { config, pkgs, ...}:
+let 
+dotPath = builtins.getEnv("XDG_DOTFILES");
+in 
 {
    programs.mpv = {
 	enable = true;
-	package = pkgs.wrapMpv (pkgs.mpv-unwrapped.override { vapoursynthSupport = true; }) { youtubeSupport = true;  }
-   }
+	bindings = builtins.readFile "${dotPath}/common/home/programs/input_mpv.conf";
+	package = pkgs.wrapMpv (pkgs.mpv-unwrapped.override { vapoursynthSupport = true; }) 
+	{ 
+	youtubeSupport = true; 
+	scripts = with pkgs.mpvScripts; [mpris seekTo cutter autoload autocrop thumbfast quality-menu mpv-playlistmanager blacklistExtensions ];
+	};
+   };
 }
