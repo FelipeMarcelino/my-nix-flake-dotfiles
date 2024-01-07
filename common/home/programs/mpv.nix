fb5a236073f2747f49b7e45c2d30f1f2678ae877
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2024-01-07 00:43:55 -0300

    Some clean

--- common/home/programs/mpv.nix
+++ common/home/programs/mpv.nix
@@ -62,13 +62,7 @@
 		"S" = "script-binding uosc/stream-quality";
 		"r" = "script-binding uosc/shuffle";
 	};
-	#package = pkgs.wrapMpv (pkgs.mpv-unwrapped.override { vapoursynthSupport = true; }) 
-	#{ 
-	#youtubeSupport = true; 
-	#scripts = with pkgs.mpvScripts; [mpris uosc seekTo cutter autoload autocrop thumbfast quality-menu mpv-playlistmanager blacklistExtensions ];
-	#};
 	package = pkgs.mpv;
-
 	scripts = with pkgs.mpvScripts; [mpris uosc seekTo cutter autoload autocrop thumbfast quality-menu mpv-playlistmanager blacklistExtensions ];
 	config = {
 		gpu-api="vulkan";
